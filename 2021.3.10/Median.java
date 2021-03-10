3bf27fda31fef6931a23f35c36924acfdaff49bc
brianjien <62172612+brianjien@users.noreply.github.com>
2021-03-10 23:54:59 +0800

    Update Median.java

--- 2021.3.10/Median.java
+++ 2021.3.10/Median.java
@@ -5,41 +5,15 @@ public class prog5 {
 
     public static void main(String[]args){
 
-        float num,num2,num3,num4,num5,num6,num7,num8,num9,num10,a,b,c,d,e,f,g,h,y,u;
-
-
         Scanner brian = new Scanner(System.in);
-        System.out.println("input number1 ");
-        num = brian.nextFloat();
-        a = num;
-        System.out.println("input number2 ");
-        num2 = brian.nextFloat();
-        b = num2;
-        System.out.println("input number3 ");
-        num3 = brian.nextFloat();
-        c = num3;
-        System.out.println("input number4 ");
-        num4 = brian.nextFloat();
-        d = num4;
-        System.out.println("input number5 ");
-        num5 = brian.nextFloat();
-        e = num5;
-        System.out.println("input number6 ");
-        num6 = brian.nextFloat();
-        f = num6;
-        System.out.println("input number7 ");
-        num7 = brian.nextFloat();
-        g = num7;
-        System.out.println("input number8 ");
-        num8 = brian.nextFloat();
-        h = num8;
-        System.out.println("input number9 ");
-        num9 = brian.nextFloat();
-        y = num9;
-        System.out.println("input number10 ");
-        num10 = brian.nextFloat();
-        u = num10;
-        float score[] = {a,b,c,d,e,f,g,h,y,u};
+        float []score=new float[11];
+        for(int i=0;i<10;i++){
+
+            System.out.println("input number "+(i+1));
+            score[i]=brian.nextFloat();
+
+        }
+
         for (int x = 1; x < score.length; x++)
         {
             for (int p = 0 ; p < score.length; p++)
@@ -65,3 +39,4 @@ public class prog5 {
 
 
 }
+
